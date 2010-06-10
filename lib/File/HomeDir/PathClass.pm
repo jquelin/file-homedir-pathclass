e2c2e7eb6e061bb1d4159b878a1e8e0af15d007f
Jérôme Quelin <jquelin@gmail.com>
2010-06-10 13:06:52 +0200

    initial wrapping

--- lib/File/HomeDir/PathClass.pm
+++ lib/File/HomeDir/PathClass.pm
@@ -5,5 +5,22 @@ use warnings;
 package File::HomeDir::PathClass;
 # ABSTRACT: File::HomeDir returning Path::Class objects
 
+use File::HomeDir ();
+use Path::Class;
+use Sub::Exporter -setup => {
+    exports => [ @File::HomeDir::EXPORT_OK ],
+    #groups => {    },
+};
+
+
+foreach my $sub ( @File::HomeDir::EXPORT_OK ) {
+    no strict 'refs';
+    *{ $sub } = sub {
+        shift if $_[0] eq __PACKAGE__;
+        my $result = "File::HomeDir::$sub"->(@_);
+        return dir( $result );
+    };
+}
+
 1;
 __END__
